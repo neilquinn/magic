31a7472158d66a77e977b0239832846dbf4c907f
Quinn <nquinn@hqnquinn901.cvent.net>
2018-02-28 17:31:41 -0500

    first project files

---
+++ Magic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic
+//
+//  Created by Quinn, Neil on 2/28/18.
+//  Copyright © 2018 Quinn, Neil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
